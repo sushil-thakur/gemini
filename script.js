32fa7615a9decb58eb9a789c0dafaeff36fc841f
sushil <sushilt2059@gmail.com>
2024-10-25 17:03:17 +0545

    js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const typingform = document.querySelector(".typing-form");
-typingform.addEventListener("submit",  (e) =>{
+const typingForm = document.querySelector(".typing-form");
+const chatList= document.querySelector(".chat-list");
+
+let userMessage = null;
+const createMessageElement = (content, className) => {
+    const div = document.createElement("div");
+    div.classList.add("message", className);
+
+    div.innerHTML = content;
+return div;
+}
+
+const handleOutgoingChat = () => {
+    userMessage = typingForm.querySelector(".typing-input").value.trim();
+    if(!userMessage) return;
+   const html = `<div class="message-content">
+                <img src="image/user.png" alt="User Image" class="avatar">
+                <p class="text"></p>
+            </div>`;
+ const outgoingMessageDiv = createMessageElement(html, "outgoing");
+ outgoingMessageDiv.querySelector(".text").innerText = userMessage;
+  charlist.appendChild(outgoingMessageDiv); 
+}
+
+typingForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    handleoutgoingchat();
+    handleOutgoingChat();
 });
